cb7b25142a76062581b9fc737f7c5dc7131d59b6
I-n-o-k <inok.dr189@gmail.com>
2023-08-10 19:49:15 +0700

    lime: Import Prebuilt DTB And DTBO

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,11 @@ include device/xiaomi/sm6115-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := lime,lemon,pomelo
 
+# Kernel - Prebuilt
+#BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
 # Inherit from the proprietary version
 include vendor/xiaomi/lime/BoardConfigVendor.mk
